f1cc56a2017779d8f559fe2d06746601f04288fa
Vipulbelekar <62843449+Vipulbelekar@users.noreply.github.com>
2023-11-27 18:13:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-pxhqs4
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Vipulbelekar/stackblitz-starters-pxhqs4)
---
+++ my-project/src/app/servers/servers.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-servers',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
